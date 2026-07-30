f0251ebe1cdbfae5a5290582e3fd387965ee84f8
Rishikesh7713 <rishikeshpramoth@gmail.com>
2026-07-30 18:43:56 +0530

    Add files via upload

---
+++ Image_Manipulation_ACP.py
@@ -0,0 +1,20 @@
+import cv2
+import matplotlib.pyplot as plt
+
+image=cv2.imread('Tropical beach  wallpaper desktop laptop background 4k HD 1080p Blue Ocean Palm tree travel images.jpeg')
+
+image_rgb=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+plt.imshow(image_rgb)
+plt.title("RGB Image")
+plt.show()
+
+gray_image=cv2.cvtColor(image ,cv2.COLOR_BGR2GRAY)
+plt.imshow(gray_image, cmap="gray")
+plt.title("Grayscale Image")
+plt.show()
+
+cropped_image=image[200:400, 300:600]
+cropped_rgb=cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)
+plt.imshow(cropped_rgb)
+plt.title("Cropped Region")
+plt.show()
